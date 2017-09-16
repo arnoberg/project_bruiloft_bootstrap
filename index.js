684e7d7f498f4d2535c3275c922ccba1db328154
Arno van den Berg <arvandenberg@gmail.com>
2017-09-16 14:03:44 +0200

    helped with updates stackoverflow andrei
    new index.js created.

---
+++ index.js
@@ -0,0 +1,14 @@
+/// START
+var express = require('express');
+var path = require('path')
+var app = express();        
+var port = process.env.PORT || 80;
+
+// this allows the files to be visible without having to type in the
+// index.html for the url simply do / and you'll get the url
+app.use(express.static(path.join(__dirname, 'public'),{
+  extensions: ['html']}
+));
+
+app.listen(port);
+///END
