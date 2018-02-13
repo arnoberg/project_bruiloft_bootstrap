d37efda37100ebfcc3e25d30d32f944f991fa98f
Arno van den Berg <arvandenberg@gmail.com>
2018-02-13 21:41:29 +0100

    syntax error fixed

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(express.static(path.join(__dirname, 'public'),{
 ));
 
 
-app.listen(port); => console.log(`Listening on ${ PORT }`))
+app.listen(port); => console.log(`Listening on ${ PORT }`)
 ///END
