d7292e7951d288582d41d57b3f2945d932e314d9
Arno van den Berg <arvandenberg@gmail.com>
2018-02-14 09:30:43 +0100

    many changes see onenote

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/vendor"));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
