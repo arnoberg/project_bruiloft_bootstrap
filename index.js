05412b91dd9607d0632419c6e2a15972c4e4b8ce
Arno van den Berg <arvandenberg@gmail.com>
2018-02-14 09:10:28 +0100

    index changes

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ var port = process.env.PORT || 5000;
 
 // this allows the files to be visible without having to type in the
 // index.html for the url simply do / and you'll get the url
-app.use(express.static(path.join(__dirname, 'public'),{
-  extensions: ['html']}
-));
+app.use(express.static(path.join(__dirname, '/index.html')));
 
-app.get('/','index.html');
+//app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port);
