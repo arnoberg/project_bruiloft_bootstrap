d6ef98e337e63a4479440dfa0974bcbc94ad12e5
Arno van den Berg <arvandenberg@gmail.com>
2018-02-14 21:10:36 +0100

    changed node.js to savethedate

--- index.js
+++ index.js
@@ -9,12 +9,8 @@ app.use('/public', express.static(path.join(__dirname + "/public")));
 app.use('/vendor', express.static(path.join(__dirname + "/vendor")));
 
 
-//app.use(express.static('public'));
-//app.use(express.static('vendor'));
-
-
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/savethedate.html'));
 });
 
 
